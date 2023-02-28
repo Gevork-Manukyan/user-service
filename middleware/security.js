bf38d61659e1908edb08101a5bab675597e049f2
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-02-28 13:10:56 -0800

    creates extractJwtHeader function

--- middleware/security.js
+++ middleware/security.js
@@ -1,2 +1,15 @@
 const jwt = require("jsonwebtoken")
 const { SECRET_KEY } = require("./config");
+
+
+// create a function to extract the JWT from the request header
+function extractJwtHeader ({ headers }) {
+    if (headers?.authorization) {
+        const [scheme, token] = headers.authorization.split(" ");
+        if (scheme.trim() === "Bearer") {
+            return token;
+        }
+    }
+
+    return null
+}
