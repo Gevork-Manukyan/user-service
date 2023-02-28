db4bf7ad33c5bbbc23a06677c27654c27aada6f7
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-02-28 14:33:43 -0800

    creates register endpoint

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require("../models/user.model")
+
+function register (credentials) {
+
+}
+
+module.exports = {
+    register
+}
---
+++ routes/user.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const tokens = require('../utils/tokens');
+const security = require('../middleware/security');
+const router = express.Router();
+const { register } = require("../controllers")
+
+
+router.post("/register", async (req, res, next) => {
+    try {
+        const user = await register(req.body)
+        const token = tokens.createUserJwt(user)
+        return res.status("201").json({ user, token })
+    } catch (error) {
+        next(error)
+    }
+})
