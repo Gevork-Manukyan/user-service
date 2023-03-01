9ab5fc0e1dd385727e501bee642c94c744dcc5ba
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-01 13:00:08 -0800

    creates register function in controller Adds input validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,37 @@
 const User = require("../models/user.model")
+const { BadRequestError } = require('../utils/errors');
 
-function register (credentials) {
+async function register (credentials) {
+    const requiredFields = ['email', 'password', 'firstName', 'lastName'];
+    requiredFields.forEach((field) => {
+        if (!credentials?.hasOwnProperty(field)) {
+            throw new BadRequestError(`Missing ${field} in request body.`);
+        }
+    })
+
+    const firstName = credentials.firstName
+    const lastName = credentials.lastName
+    const email = credentials.email
+    const password = credentials.password
+
+    // Check for valid inputs
+    if (firstName.trim() === "" || lastName.trim() === "") throw new BadRequestError(`Invalid first or last name.`)
+    if (email.indexOf('@') <= 0) throw new BadRequestError('Invalid Email.');
+    if (password.trim() === "") throw new BadRequestError('Invalid Password')
+
+    // Check if user exists with this email
+    const existingUser = await User.findOne({ email })
+    if (existingUser) throw new BadRequestError(`A user already exists with email: ${email}`);
+
+    
+    
+    const user = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+    })
+
 
 }
 
