1ae392481ab7ad3b277099f57f11fe8d49f1637e
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-01 13:01:40 -0800

    installs bcrypt and adds variable to config file

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ require("dotenv").config()
 
 const PORT = process.env.PORT
 const SECRET_KEY = process.env.JWT_SECRET
-
+const BCRYPT_WORK_FACTOR = 13
 
 module.exports = {
     PORT,
-    SECRET_KEY
+    SECRET_KEY,
+    BCRYPT_WORK_FACTOR
 }
