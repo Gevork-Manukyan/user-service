c6ad8f88a9b6137fe075a9468811eb4425e9432e
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-08 18:05:59 -0800

    adds error handling routes

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const { PORT } = require("./config");
 const express = require('express');
+const security = require("./middleware/security")
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+
+app.use(cors());
+app.use(express.json());
+app.use(security.extractUserFromJwt);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use((req, res, next) => {
+  return next(new NotFoundError())
+})
+
+/** Generic error handler; anything unhandled goes here. */
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message
+
+  return res.status(status).json({
+    error: { message, status },
+  })
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
