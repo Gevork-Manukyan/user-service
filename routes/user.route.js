becd616907ad41bf65f5b7eb3badbf00bd0599ad
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-15 23:43:33 -0700

    creates login route

--- routes/user.route.js
+++ routes/user.route.js
@@ -14,4 +14,18 @@ router.post("/register", async (req, res, next) => {
     }
 })
 
+router.post("/login", async (req, res, next) => {
+    try {
+      const user = await login(req.body);
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+      const token = tokens.createUserJwt(user);
+      return res.status(200).json({ user, token });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+
 module.exports = router
